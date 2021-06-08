c415f3bf77dbce0cca73442e1d586964ee6621a3
João Victor Simonassi <jsimonassi@id.uff.br>
2021-06-08 01:22:29 -0300

    Adicionando métodos do firebase a página de admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,19 +1,97 @@
-import Head from 'next/head';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from '../components/Header/Header';
-import Background from '../components/IndexComponents/Background/Background';
-import Video from '../components/IndexComponents/Video/Video';
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import {
+  Card, CardBody, Button
+} from 'reactstrap';
+import { uploadImagesToStorage } from '../lib/Api';
 
 export default function Home() {
+
+  const [fileList, setFileList] = useState([]);
+  const [nameValue, setNameValue] = useState("");
+  const [pathValue, setPathValue] = useState("");
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const onChangeNameHandler = event => {
+    setNameValue(event.target.value);
+  };
+
+  const onChangePathHandler = event => {
+    setPathValue(event.target.value);
+  };
+
+  const onChangeUsernameHandler = event => {
+    setUsernameValue(event.target.value);
+  };
+
+  const onChangePasswordHandler = event => {
+    setPasswordValue(event.target.value);
+  };
+
+  const onDrop = useCallback(acceptedFiles => {
+    console.log("Aceitei esses arquivos: ", acceptedFiles);
+    setFileList(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  const uploadInfos = () => {
+    console.log("Fui clicado!", fileList, nameValue, pathValue);
+    uploadImagesToStorage(fileList, pathValue, nameValue, usernameValue, passwordValue);
+  }
+
+
   return (
-    <div>
-      <Head>
-        <title>2Pixels Fotografia</title>
-        <meta name='robots' content='index/follow' />
-        <meta name='description' content='Registrando momentos em Itaboraí e região' />
-      </Head> 
-     <h4>Upload de arquivos - 2Pixels Fotografia</h4>
-     
+    <div >
+      <h1>Upload de arquivos - 2Pixels Fotografia</h1>
+      <Card>
+        <div style={{ backgroundColor: 'black', height: '50px' }} {...getRootProps()}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p style={{ color: 'white' }}>Solte os arquivos aqui</p> :
+              <p style={{ color: 'white' }}>Arraste os novos arquivos , ou clique aqui</p>
+          }
+        </div>
+        <CardBody>
+          <br />
+          <label>Nome: </label>
+          <input
+            type="text"
+            name="name"
+            onChange={onChangeNameHandler}
+            value={nameValue}
+          />
+          <br />
+          <label>Path: </label>
+          <input
+            type="text"
+            name="path"
+            onChange={onChangePathHandler}
+            value={pathValue}
+          />
+          <br />
+          <br />
+          <label>Username: </label>
+          <input
+            type="text"
+            name="username"
+            onChange={onChangeUsernameHandler}
+            value={usernameValue}
+          />
+          <br />
+          <label>Password: </label>
+          <input
+            type="text"
+            name="password"
+            onChange={onChangePasswordHandler}
+            value={passwordValue}
+          />
+          <br />
+          <Button onClick={uploadInfos} >Enviar</Button>
+        </CardBody>
+      </Card>
     </div>
-  );
+  )
 }
